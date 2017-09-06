524dc85c0506b91325daff913b435e090e07a859
Daniel Thomas <drt24@cam.ac.uk>
2017-09-06 09:59:40 +0100

    Don't use deprecated force parameter for concat

--- manifests/server/config.pp
+++ manifests/server/config.pp
@@ -142,7 +142,6 @@ class postgresql::server::config {
     concat { $pg_ident_conf_path:
       owner  => $user,
       group  => $group,
-      force  => true, # do not crash if there is no pg_ident_rules
       mode   => '0640',
       warn   => true,
       notify => Class['postgresql::server::reload'],
@@ -153,7 +152,6 @@ class postgresql::server::config {
     concat { $recovery_conf_path:
       owner  => $user,
       group  => $group,
-      force  => true, # do not crash if there is no recovery conf file
       mode   => '0640',
       warn   => true,
       notify => Class['postgresql::server::reload'],
